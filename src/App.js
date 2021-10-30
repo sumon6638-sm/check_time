f7d6cc6144529a2dd87e9f128d8c8a02797f2e23
sumon6638-sm <mdsumoncse19@gmail.com>
2021-10-30 07:02:03 +0600

    check_time_2

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Hello world</h2>
+      <h4>Updated Time_12hr back</h4>
+      <h4>Updated Time_real</h4>
+      <h4>Updated Time_real</h4>
+      <h4>Updated Time_fake</h4>
+      <h2>Update</h2>
+      <h2>6days</h2>
+    </div>
+  );
+}
+
+export default App;
